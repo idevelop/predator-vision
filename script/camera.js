12eb87ff2ae4a9e0f2f4fe30ffd0572dd5fc8119
Andrei Gheorghe <andreig@gmail.com>
2013-02-25 23:50:29 +0200

    Firefox on dual-GPU Macbooks fix
    See: https://bugzilla.mozilla.org/show_bug.cgi?id=713305

--- script/camera.js
+++ script/camera.js
@@ -46,8 +46,10 @@ var camera = (function() {
 
 		var drawInterval = Math.round(1000 / options.fps);
 		var draw = function() {
-			context.drawImage(video, 0, 0, video.width, video.height);
-			options.onFrame(canvas);
+			try {
+				context.drawImage(video, 0, 0, video.width, video.height);
+				options.onFrame(canvas);
+			} catch (e) { }
 
 			setTimeout(draw, drawInterval);
 		};
